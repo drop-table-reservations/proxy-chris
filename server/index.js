e94e193d6164448f7646296e4eba90ff66473636
Chris Larson <c.lars701@gmail.com>
2018-09-18 13:48:40 -0700

    add fixes to get any restaurant

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-app.use(express.static('public'));
+app.use('/restaurants/:id', express.static('public'));
+
+// app.get('/:id', (req, res) => {
+//   console.log(req.params.id);
+// });
+
 
 app.listen(3000, () => {console.log('Listening on port 3000')});
